fd68851d63e8063e1d13b4c0853617fdc6a6d0c5
Alex Reyes <alex.reyes@listrak.com>
2017-06-12 09:50:51 -0400

    First Commit

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace JsonApiClient
+{
+    public static class Program
+    {
+        public static void Main(String[] args) {
+            WeatherApiClient.GetWeatherForecast();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication3/WeatherApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace JsonApiClient
+{
+    public static class WeatherApiClient
+    {
+        public static void GetWeatherForecast()
+        {
+            Console.Write("Name a city");
+            string city = Console.ReadLine();
+
+            string jsonCurrentWeather = AccessWebPage.HttpGet("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=47992ad1b3261b707350bf13aac83023");
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(WeatherData.CurrentRoot));
+
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(WeatherData));
+
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
+            {
+                // deserialize the JSON object using the WeatherData type.
+                var weatherData = (WeatherData)serializer.ReadObject(ms);
+                Console.WriteLine(weatherData);
+            }
+        }
+    }
+}
